8fef9306a7c424e7d10505a5b6d0be17d5d8da0f
scarlett014j <scarlett014j@gmail.com>
2020-10-13 18:42:41 -0400

    added something

---
+++ stacked_bar/stacked_bar.js
@@ -0,0 +1,26 @@
+d3.json("full_drinks.json").then(function(drinkData) {
+    console.log(drinkData);
+    });
+
+
+
+
+
+
+
+
+// d3.json("full_drinks.json").then(function(csvData) {
+//     csvData.forEach(function(data) {
+//         var i;
+//         for(i = 1; i <= 15; i++) {
+//             if (data[`ingredient_${i}`]) {
+//                 ingredientsSelectedMap[data[`ingredient_${i}`].toLowerCase()] = false;
+//                 if (data[`ingredient_${i}`].toLowerCase() in ingredientsToDrinkMap) {
+//                     var drinkList = ingredientsToDrinkMap[data[`ingredient_${i}`].toLowerCase()];
+//                     ingredientsToDrinkMap[data[`ingredient_${i}`].toLowerCase()] = [...drinkList, data.name];
+//                 } else {
+//                     ingredientsToDrinkMap[data[`ingredient_${i}`].toLowerCase()] = [data.name];
+//                 }
+//             }
+//         }
+//      });
