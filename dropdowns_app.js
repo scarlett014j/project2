3da1919b655206e0f7d8eeafee85544390a27ca1
Allison Howe <allisonhowe77@gmail.com>
2020-10-13 23:08:10 -0400

    more dropdown updates

--- dropdowns_app.js
+++ dropdowns_app.js
@@ -1,9 +1,12 @@
-let ingredientsSelectedMap = {};
 let ingredientsToDrinkMap = {};
 let drinksToIngredients = {};
 let drinks = [];
 let drinks2 = [];
 let drinks3 = [];
+let selectedItem1 = "";
+let selectedItem2 = "";
+let selectedItem3 = "";
+
 
 let dropdown3 = new Set();
 
@@ -13,7 +16,6 @@ d3.csv("full_drinks_copy.csv").then(function(csvData) {
         var i;
         for(i = 1; i <= 15; i++) {
             if (data[`ingredient_${i}`]) {
-                ingredientsSelectedMap[data[`ingredient_${i}`].toLowerCase()] = false;
                 if (data[`ingredient_${i}`].toLowerCase() in ingredientsToDrinkMap) {
                     var drinkList = ingredientsToDrinkMap[data[`ingredient_${i}`].toLowerCase()];
                     ingredientsToDrinkMap[data[`ingredient_${i}`].toLowerCase()] = [...drinkList, data.name];
@@ -25,22 +27,17 @@ d3.csv("full_drinks_copy.csv").then(function(csvData) {
         }
         drinksToIngredients[data.name.toLowerCase()] = ingredients;
     }); 
+
     d3.select("#ingredients1")
         .selectAll('option')
-        .data(Object.keys(ingredientsSelectedMap))
-        .enter()
-        .append('option')
-        .text(ingr => ingr)
-        .attr("value", ingr => ingr);
-    d3.select("#ingredients3")
-        .selectAll('option')
-        .data(dropdown3.keys())
+        .data(["Select an ingredient", ...Object.keys(ingredientsToDrinkMap).sort()])
         .enter()
         .append('option')
         .text(ingr => ingr)
         .attr("value", ingr => ingr);
 });
 function onSelect(selectedItem) {
+    selectedItem1 = selectedItem.value;
     let dropdown = new Set();
     drinks = ingredientsToDrinkMap[selectedItem.value];
     var i;
@@ -50,12 +47,14 @@ function onSelect(selectedItem) {
             dropdown.add(drinksToIngredients[drinks[i].toLowerCase()][j].toLowerCase());
         }
     }
+    dropdown.delete(selectedItem1)
+    dropdown.delete(selectedItem3)
     d3.select("#ingredients2")
         .selectAll('option')
         .remove()
     d3.select("#ingredients2")
         .selectAll('option')
-        .data(Array.from(dropdown))
+        .data(["Select an ingredient", ...Array.from(dropdown).sort()])
         .enter()
         .append('option')
         .text(ingr => ingr)
@@ -70,7 +69,11 @@ function onSelect(selectedItem) {
         .text(drink => drink)
 }
 function onSelect2(selectedItem) {
-    drinks2 = drinks.filter(value => ingredientsToDrinkMap[selectedItem.value].includes(value))
+    selectedItem2 = selectedItem.value;
+    drinks2 = drinks.filter(value => {
+        const drink = ingredientsToDrinkMap[selectedItem.value];
+        return drink == null ? [] : drink.includes(value);
+    });
 
     let dropdown = new Set();
     var i;
@@ -80,12 +83,14 @@ function onSelect2(selectedItem) {
             dropdown.add(drinksToIngredients[drinks2[i].toLowerCase()][j].toLowerCase());
         }
     }
+    dropdown.delete(selectedItem1)
+    dropdown.delete(selectedItem2)
     d3.select("#ingredients3")
         .selectAll('option')
         .remove()
     d3.select("#ingredients3")
         .selectAll('option')
-        .data(Array.from(dropdown))
+        .data(["Select an ingredient", ...Array.from(dropdown).sort()])
         .enter()
         .append('option')
         .text(ingr => ingr)
@@ -97,15 +102,42 @@ function onSelect2(selectedItem) {
         .data(drinks2)
         .enter()
         .append('li')
-        .text(drink => drink)
+        .text(drink => drink);
 }
 function onSelect3(selectedItem) {
-    drinks3 = drinks2.filter(value => ingredientsToDrinkMap[selectedItem.value].includes(value))
+    selectedItem3 = selectedItem.value;
+    drinks3 = drinks2.filter(value => {
+        const drink = ingredientsToDrinkMap[selectedItem.value];
+        return drink == null ? [] : drink.includes(value);
+    });
     d3.selectAll('li').remove()
     d3.select("#drinks")
         .selectAll('li')
         .data(drinks3)
         .enter()
         .append('li')
-        .text(drink => drink)
+        .text(drink => drink);
+}
+
+function resetSelections() { 
+    document.getElementById("ingredients1").innerHTML = 
+        null; 
+    document.getElementById("ingredients2").innerHTML = 
+        null; 
+    document.getElementById("ingredients3").innerHTML = 
+        null; 
+    document.getElementById("drinks").innerHTML = 
+        null; 
+
+    d3.select("#ingredients1")
+        .selectAll('option')
+        .data(["Select an ingredient", ...Object.keys(ingredientsToDrinkMap).sort()])
+        .enter()
+        .append('option')
+        .text(ingr => ingr)
+        .attr("value", ingr => ingr);
+    
+    selectedItem1 = "";
+    selectedItem2 = "";
+    selectedItem3 = "";
 }
